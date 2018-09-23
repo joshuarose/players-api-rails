4afd805f0ad77c338bbf301c8f7a4db3e6a99ec7
Joshua Rose <joshuarose@outlook.com>
2018-09-23 11:00:49 -0400

    Updated factorybot deprecations

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
   factory :player do
-    first_name "Ma"
-    last_name "Long"
-    rating 9000
-    handedness "right"
+    first_name { "Ma" }
+    last_name { "Long" }
+    rating { 9000 }
+    handedness { "right" }
   end
   factory :player_two, class: Player do
-    first_name "Timo"
-    last_name "Boll"
-    rating 8000
-    handedness "right"
+    first_name { "Timo" }
+    last_name { "Boll" }
+    rating { 8000 }
+    handedness { "right" }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,16 +1,16 @@
 FactoryBot.define do
   factory :user do
-    first_name "Jim"
-    last_name  "Bob"
-    email "jim@bob.com"
-    password "foobar123"
-    password_confirmation "foobar123"
+    first_name { "Jim" }
+    last_name  { "Bob" }
+    email { "jim@bob.com" }
+    password { "foobar123" }
+    password_confirmation { "foobar123" }
   end
   factory :user_two, class: User do
-    first_name "Joe"
-    last_name  "Bob"
-    email "joe@bob.com"
-    password "foobar123"
-    password_confirmation "foobar123"
+    first_name { "Joe" }
+    last_name  { "Bob" }
+    email { "joe@bob.com" }
+    password { "foobar123" }
+    password_confirmation { "foobar123" }
   end
 end
